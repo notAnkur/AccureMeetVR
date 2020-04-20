9e4a70ba19daff478450d05c8f30f92ebcdf23a3
Ankur Anant <ankuranant99@gmail.com>
2020-04-20 11:47:19 +0530

    fixed the audio issue

---
+++ Assets/Scripts/SingleUse/LoadCardboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class LoadCardboard : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(LoadDevice("cardboard"));
+    }
+
+    IEnumerator LoadDevice(string newDevice)
+    {
+        UnityEngine.XR.XRSettings.LoadDeviceByName(newDevice);
+        yield return null;
+        UnityEngine.XR.XRSettings.enabled = true;
+    }
+}
